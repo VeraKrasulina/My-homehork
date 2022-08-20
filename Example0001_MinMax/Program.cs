06b87668edccd25d00d4b0e7b5906c69811dffa7
Vera Krasulina <kshisinka@hotmail.com>
2022-08-20 19:00:03 +0300

    Example Min_Max between 2 numbers

---
+++ Example0001_MinMax/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите первое число: ");
+double number1 = double.Parse (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+double number2 = double.Parse (Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine ($"По результатам сравнения {number1} > {number2}");
+}
+else Console.WriteLine ($"По результатам сравнения {number2} > {number1}");
