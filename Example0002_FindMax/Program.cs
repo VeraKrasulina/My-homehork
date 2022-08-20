ccd22e802c849a79aa215ebe01822472c34a5e9f
Vera Krasulina <kshisinka@hotmail.com>
2022-08-20 21:15:05 +0300

    Added git ignore

---
+++ Example0002_FindMax/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray (int [] arrey)
+{
+int length = arrey.Length;
+int index = 0;
+
+while (index < length)
+{
+    arrey[index] = new Random ().Next (-100,100);
+    index++;
+}
+}
+
+void PrintArrey (int [] list)
+{
+    int count = list.Length;
+    int pos = 0;
+
+    while (pos < count)
+    {
+        Console.WriteLine (list[pos]);
+        pos++;
+    }
+}
+
+// Fill paramether new int [????] to create a different quantity of numbers within massive
+int[] numbers = new int [3];
+FillArray (numbers);
+
+int ind = 0;
+int max = numbers[ind];
+int length = numbers.Length;
+while (ind < length)
+{
+    if (max < numbers[ind])
+    {
+        max = numbers [ind];
+    }
+ind = ind+1;
+}
+
+Console.WriteLine ("Из представленных чисел: ");
+PrintArrey (numbers);
+Console.WriteLine ($"{max} является максимальным.");
