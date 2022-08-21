99d3e82c19f6fa2c6450c88160674b72c090c667
Vera Krasulina <kshisinka@hotmail.com>
2022-08-21 10:50:14 +0300

    Even number test added

---
+++ Example0003_Even_number_test/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write ("Введите число для проверки: ");
+int number = int.Parse (Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ($"{number} является четным числом.");
+}
+else
+Console.WriteLine ($"{number} не является четным числом.");
