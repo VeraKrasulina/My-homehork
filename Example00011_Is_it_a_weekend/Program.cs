a81cbf66e1f21fc15872a51a2dc565aeb0ac2568
Vera Krasulina <kshisinka@hotmail.com>
2022-08-26 10:09:40 +0300

    Id it a weekend? Check

---
+++ Example00011_Is_it_a_weekend/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер дня недели: ");
+int num = int.Parse (Console.ReadLine());
+if (num > 7)
+{
+    Console.WriteLine("Некорректное значение");
+}
+else if (num > 5)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
