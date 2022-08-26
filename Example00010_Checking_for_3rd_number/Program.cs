d3e0fc5007ceecf9c4cbf2009f0f5bc5630ce199
Vera Krasulina <kshisinka@hotmail.com>
2022-08-26 10:13:35 +0300

    added 2 more homeworks

---
+++ Example00010_Checking_for_3rd_number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string num = Console.ReadLine();
+char [] array = num.ToCharArray();
+int length = array.Length;
+if (length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(num[2]);
+}
