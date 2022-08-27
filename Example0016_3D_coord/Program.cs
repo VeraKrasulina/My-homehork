da102bfbbb289f4472d6ce4c691271ef358b0a18
Vera Krasulina <kshisinka@hotmail.com>
2022-08-27 10:49:56 +0300

    Finding distance in 3D coords

---
+++ Example0016_3D_coord/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+Console.Write(" X:  ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Y:  ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Z:  ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write(" X:  ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Y:  ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write(" Z:  ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt((x2 - x1) + (y2 - y1) + (z2 - z1));
+Console.WriteLine($"Расстояние между первой и второй точкой в пространстве равняется {result}");
