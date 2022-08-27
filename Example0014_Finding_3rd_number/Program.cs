633e86cc1ed7a9eb13cd5b8b450e235686c1ecb2
Vera Krasulina <kshisinka@hotmail.com>
2022-08-27 09:35:42 +0300

    Finding 3rd number

---
+++ Example0014_Finding_3rd_number/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <100) Console.WriteLine ("Введенное число не содержит третьего числа.");
+int num1 = 0;
+
+while (num > 1000)
+{
+    num = num / 10;
+}
+int num3 = num % 10;
+
+Console.WriteLine (num3);
