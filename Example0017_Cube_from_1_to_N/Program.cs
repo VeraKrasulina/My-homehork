02e87ab4e4030df0b3b8f508132729e69ae2bbba
Vera Krasulina <kshisinka@hotmail.com>
2022-08-27 12:05:17 +0300

    Cubes from 1 to N

---
+++ Example0017_Cube_from_1_to_N/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int b = 0;
+while (a <= num)
+{
+    b = Convert.ToInt32 ( Math.Pow (a, 3));
+    Console.Write ($"{b} ");
+    a++;
+}
