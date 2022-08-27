97ae7ea1f82130bcca68ca8e2c18105c5938ce74
Vera Krasulina <kshisinka@hotmail.com>
2022-08-27 09:09:31 +0300

    Finding 2nd number math

---
+++ Example0013_Finding_2nd_number/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <100 | num > 999) Console.WriteLine ("Введенное число не трехзначное.");
+// int hundreds = num / 100;
+int ones = num % 10;
+int tens = ((num % 100) - ones) / 10;
+
+Console.WriteLine($"Второе число в этой цифре - {tens}");
